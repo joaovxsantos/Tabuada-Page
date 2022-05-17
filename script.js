58e6924d97b582203e9f9baba90ad07b2a8097b7
João Vitor <joaovitor55670@gmail.com>
2022-05-16 23:40:55 -0300

    commit funcionou

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function tabuada(){
-    let valor = document.getElementById("valor")
-    let x = document.getElementById("a")
-    let a = valor.value
-    var teste = ""
-    for (var count = 1; count<=10; count++){
-   teste = a + " x " + count + " = " + a*count
+    let valor = document.getElementById("valor") //aqui peguei o elemento com id "valor"
+    let x = document.getElementById("a") //aqui peguei o elemento com id "a"
+    let a = valor.value //aqui peguei o value do let valor
+    var teste = "" //aq uma var vazia pra usar dps
+    for (var count = 1; count<=10; count++){ //aqui o for
+   teste += a + " x " + count + " = " + a*count + "<br/>" //aq eu apenas add tudo q fiz no for **obs para o += q é para incrementar tudo
 
-   x.innerHTML = teste
+   x.innerHTML = teste //aq eu passei o var teste para o innerhtml de x
 }
 }
 
